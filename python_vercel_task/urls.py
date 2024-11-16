f34230f5eb6add155aa932fa8d6d7bcf4e61a085
Esteban <ehequiel911@gmail.com>
2024-11-15 22:45:43 -0300

    ds

--- python_vercel_task/urls.py
+++ python_vercel_task/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+
+def home(request):
+    return HttpResponse("¡Bienv¿")
+
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
 ]
